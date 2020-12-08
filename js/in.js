3c2d02514a56643258d9d23a1c22986ded1935ec
joe-wallace23 <60151170+joe-wallace23@users.noreply.github.com>
2020-12-08 11:52:08 -0600

    Add files via upload

---
+++ js/in.js
@@ -0,0 +1,24 @@
+ for (let number = 0; number <= 100; number++){
+      if (number === 3){
+          console.log('MSU');
+      }
+     if(number === 5){
+         console.log('MSUBears');
+      }
+      console.log('loops' + number)
+ }
+ document.write('bear')
+ 
+
+// let first= 0
+// let second =1
+// let result= second
+
+// for (let numberw=1; numberw<=100; numberw++){
+//     document.write(result + "<br/>");
+//     result= first+second
+//     first=second
+//     second=result;
+
+// }
+
